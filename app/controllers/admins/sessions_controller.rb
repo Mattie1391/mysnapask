c88316e77c7bc418a3731227971728aecb9a40c0
Nikka <kyra11024@gmail.com>
2022-07-01 05:15:18 +0800

    add admin session

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -14,9 +14,10 @@ class Admins::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    # super
+    redirect_to root_path
+  end
 
   # protected
 
